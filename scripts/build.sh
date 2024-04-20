34641cfb4c7254b400082d782448db2a5ab6f71f
Vladimir Fedorov <fedorovvs@gmail.com>
2024-04-20 23:30:41 +0200

    Increase build number on every build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,5 @@
+FILE=./source/pdxinfo
+BUILD_NUMBER=$(grep 'buildNumber=' $FILE | sed 's/[^0-9]*//g')
+NEW_BUILD_NUMBER=$((BUILD_NUMBER + 1))
+sed -i '' "s/buildNumber=$BUILD_NUMBER/buildNumber=$NEW_BUILD_NUMBER/" $FILE
 pdc -sdkpath ~/Developer/tools/PlaydateSDK ./source ./build/Lighthouse.pdx
